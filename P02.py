4c4824e1e60405a8c0fb3acfca7e0473fff46d31
NayanPython <choudharyji527@gmail.com>
2026-06-08 23:00:12 +0530

    P02.1

---
+++ P02.py
@@ -0,0 +1,17 @@
+#  HASH SETS
+
+# I want you to write the function find_duplicate_fast(arr).
+#
+# Instead of two loops, you will only use one loop.
+#
+# Create your empty notebook (a Python set) before the loop starts.
+#
+# Start your for loop to look at each number in the array.
+#
+# Inside the loop, write an if statement: check if the current number is already in the notebook.
+#
+# If it is, return that number. You found the clone.
+#
+# If it is not, .add() that number to the notebook so you remember it for later.
+
+
