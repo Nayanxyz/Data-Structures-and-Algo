e59aecd6903553cd61b15e853870bd6473a6404d
NayanPython <choudharyji527@gmail.com>
2026-06-10 23:13:18 +0530

    P07.3

--- P07.py
+++ P07.py
@@ -27,4 +27,11 @@ node_x = Node(15)
 # Step 1: Secure the rest of the list
 node_x.next = node_a.next
 
+# Step 2: Break the old bridge and connect it to the new node
+node_a.next = node_x
 
+# --- 3. THE PROOF (TRAVERSAL) ---
+current = node_a
+while current is not None:
+    print(current.value)
+    current = current.next
