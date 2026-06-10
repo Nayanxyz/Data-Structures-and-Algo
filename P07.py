6a57c952868b8a5f043973e7135a854380245808
NayanPython <choudharyji527@gmail.com>
2026-06-10 23:12:56 +0530

    P07.1

---
+++ P07.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+node_a = Node(10)
+node_b = Node(20)
+
+node_a.next = node_b
+
+node_x = Node(15)
+    # The new stone, currently pointing to nothing
+
+
