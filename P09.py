a108c230fed2c6186f302ac673e9904d7d644cc7
NayanPython <choudharyji527@gmail.com>
2026-06-11 21:48:04 +0530

    P09.1

---
+++ P09.py
@@ -0,0 +1,26 @@
+# Module 4: Queues (FIFO)
+# We are immediately moving to the exact opposite constraint.
+#
+# If a Stack is a pile of heavy plates, a Queue is a line of people waiting at a coffee shop.
+# It operates on First In, First Out (FIFO).
+#
+# The first person who gets in line is the first person who gets their coffee.
+#
+# You cannot skip the line.
+#
+# If you build a server that handles print jobs, or a matchmaking system for a multiplayer game, you use a Queue. You want the person who has been waiting the longest to be served first.
+#
+# The Challenge: The Printer Queue
+# You are going to build a PrinterQueue class.
+# It is almost identical to the structure of your BrowserHistory, but the physical removal constraint is on the opposite end of the data.
+#
+# When a document is added, it goes to the back of the line (.append()).
+# When a document is printed, it must be removed from the FRONT of the line.
+#
+# Hint: In a Python list, .pop() removes the last item. If you want to remove the very first item (Index 0), you use .pop(0).
+#
+# Here is your exact objective:
+
+# Write the PrinterQueue class. Create the __init__, add_job, and print_job methods.
+# Apply the FIFO constraint. Paste the code.
+
