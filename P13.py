bcf8acdda31a9f441a40dae6295fc5590ce51f3f
NayanPython <choudharyji527@gmail.com>
2026-06-12 23:23:26 +0530

    P13.1

---
+++ P13.py
@@ -0,0 +1,6 @@
+# We are tackling Invert Binary Tree. This is one of the most famous technical interview questions
+# in software engineering history. (It is famously known as the question the creator of Homebrew failed at Google).
+#
+# The Objective:
+# You must write an algorithm that acts as a mirror. Every single left map must be swapped with its right map,
+# recursively, all the way down to the bottom of the tree.
