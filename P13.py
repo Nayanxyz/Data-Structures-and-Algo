63cb4b0598df0e8f18681b6cdb6cb223b135ba16
NayanPython <choudharyji527@gmail.com>
2026-06-12 23:24:38 +0530

    P13.2

--- P13.py
+++ P13.py
@@ -4,3 +4,8 @@
 # The Objective:
 # You must write an algorithm that acts as a mirror. Every single left map must be swapped with its right map,
 # recursively, all the way down to the bottom of the tree.
+
+# Your Objective
+#
+# Once a node has swapped its own left and right arms, it must command its clones to go down the new left path
+# and the new right path and repeat the exact same process.
